b8f3ceaabf1f611ef19c1bb05be92b929abdc477
Văn Nghĩa - Love <127735777+HVNN16@users.noreply.github.com>
2026-04-14 20:46:55 +0700

    add ".env.production" to deploy and connect neon database

--- pharmacy-map/backend/db.js
+++ pharmacy-map/backend/db.js
@@ -1,11 +1,38 @@
+// import pg from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const pool = new pg.Pool({
+//   host: process.env.DB_HOST || "localhost",
+//   port: process.env.DB_PORT || 5432,
+//   user: process.env.DB_USER || "postgres",
+//   password: process.env.DB_PASSWORD || "12345",
+//   database: process.env.DB_NAME || "gisdb",
+// });
 import pg from "pg";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-export const pool = new pg.Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "12345",
-  database: process.env.DB_NAME || "gisdb",
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // Production (Neon) - sử dụng DATABASE_URL từ biến môi trường production
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  // Local development - sử dụng cấu hình .env nếu chạy local
+  pool = new pg.Pool({
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "12345",
+    database: process.env.DB_NAME || "gisdb",
+  });
+}
+
+export { pool };
