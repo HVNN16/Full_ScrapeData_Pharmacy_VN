bd68c3ec327f902bb3a8cf725c1e151838c02626
Văn Nghĩa - Love <127735777+HVNN16@users.noreply.github.com>
2026-04-14 21:50:30 +0700

    comment "Sidebar" =))

--- pharmacy-map/frontend/src/pages/admin/layout/AdminLayout.jsx
+++ pharmacy-map/frontend/src/pages/admin/layout/AdminLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./admin.css";
-import Sidebar from "./Sidebar";
+// import Sidebar from "./Sidebar";
 import AdminHeader from "./AdminHeader";
 
 export default function AdminLayout({ children }) {
