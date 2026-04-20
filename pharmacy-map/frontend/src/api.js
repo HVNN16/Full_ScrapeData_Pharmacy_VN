3afb70309b512d1dc3375802af3442954c41d931
Văn Nghĩa - Love <127735777+HVNN16@users.noreply.github.com>
2026-04-20 16:02:54 +0700

    delete comment

--- pharmacy-map/frontend/src/api.js
+++ pharmacy-map/frontend/src/api.js
@@ -1,33 +1,3 @@
-// import axios from "axios";
-
-// // KHÔNG thêm /api ở đây
-// const API_BASE_URL =
-//   process.env.REACT_APP_API_URL || "http://localhost:5000";
-
-// const api = axios.create({
-//   baseURL: `${API_BASE_URL}/api`,
-// });
-
-// // Gắn JWT token tự động
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// // =============== API ===============
-// export const fetchProvinces = async () => {
-//   const res = await api.get("/provinces");
-//   return res.data;
-// };
-
-// export const fetchGeoJSON = async (params) => {
-//   const res = await api.get("/pharmacies.geojson", { params });
-//   return res.data;
-// };
-
-// export default api;
-
 import axios from "axios";
 
 // KHÔNG thêm /api ở đây
